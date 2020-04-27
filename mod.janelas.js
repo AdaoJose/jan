03ab918329f7872e180b355bb2b25fabea711a67
Adão José <adao.jose123.a.r@gmail.com>
2020-04-27 13:31:02 -0400

    Primeiro commti

---
+++ mod.janelas.js
@@ -0,0 +1,113 @@
+/**
+ * Esta função tem por objetivo criar a navegação baseada em janelas
+ * com ela voce pode padronisar as janelas de sua aplicação vitando reescrita de codigos
+ * Ex. let home = janelas(); let about = janelas(); let menuLateral = janelas();
+ * assim voce terá uma janela para cada parte de sua aplicção que serão personalizaveis;
+ * Ex. Ola mundo com janelas();
+ * let home = janelas();
+ * home.append("Ola mundo");
+ * mais exemplos são encontrados na documentação
+ */
+
+export default function $window(){
+    let idJanela = 'JA'+parseInt(Math.random()*2000);
+    let self = {
+        name               :"",
+        title             : "Janelas JS",
+        btnBack             : true,
+        height             : '100vh',
+        width              : '100vw',
+        bg                 : '#f8f9fa',
+        bgHead             : '',
+        id                 : idJanela,
+        construct          : ()=>{
+                                if(!$('#'+self.id).length){//verifico de esta janela ja foi constrida
+                                    let html = $("<div/>",
+                                        {
+                                            'id':self.id,
+                                            'name':self.name,
+                                            'class':'janelas position-fixed z-index-7', 'style':'height:'+self.height+'; width:'+self.width+'; background-color:'+self.bg+';'
+                                        }).
+                                        append(
+                                            $('<div/>',
+                                                {'class':'chat-cabecalho cabecalho-janelas bg-primary col-12 p-3 text-light text-x-large text-center', 'style':"height:60px;"}
+                                            ).
+                                            append((self.btnBack) ? $('<i/>',{class:'fa fa-arrow-left text-x-large mr-2 bg-primary position-absolute', 'style':"left:10px;", click:()=>{self.hide()}}) : '').
+                                            append($('<span/>',{'class':'titulo-janelas'}).
+                                                append(self.title
+                                            ))
+                                        );
+                                    $('body').append(html);
+                                }else{
+                                    $('#'+self.id).
+                                    css({'height':self.height,'width': self.width,'background-color':self.bg});
+                                    $('#'+self.id+' .cabecalho-janelas').
+                                    html('').
+                                    append(
+                                        (self.btnBack) ? $('<i/>',{class:'fa fa-arrow-left text-x-large mr-2 bg-primary position-absolute', 'style':"left:10px;", click:()=>{self.hide()}}) : ''
+                                    ).
+                                    append(self.title)
+                                    ;
+                                }
+                            },
+        append            :function(val){
+                                if($('#'+self.id+' > .painel').length){
+                                    $('#'+self.id+' > .painel').append(val);
+                                }else{
+                                    $('#'+self.id).append($('<div/>',{class:'painel', 'style':'min-height:100%; min-width:100%;'}).append(val));
+                                }
+                            },
+        clearPanel    : ()=>{
+                                $('#'+self.id+' > .painel').html("");
+                            },
+        toolBArHide   :()=>{
+                            $(".janelas#"+self.id+" > .cabecalho-janelas").hide()
+        },
+        hide          : function(){
+                            $('#'+self.id).hide('slow');
+        },
+        show : function(val){
+            if(!$('#'+self.id).length){//caso seja a primeira vez chame o construct
+                self.construct();
+            }
+            $('#'+self.id).show('slow'); 
+    
+        },
+        /** 
+         * Para Realizar edição no cabeçalho janela;
+         * ao chamar o metodo o cabeçalho padrão é retirado.
+         * @param paramHTML: recebe todo squema html que será colocado no lugar do cabecalho.
+         * caso vazio o cabecalho ficará vazio tambem.
+         * @returns janela
+        */
+        setHead       :(paramHTML='')=>{
+            $(".cabecalho-janelas").html("").html(paramHTML);
+            return self;
+        },
+        setTitle     :(paramTitulo)=>{
+            $("#"+self.id+" > .cabecalho-janelas > .titulo-janelas").html(paramTitulo);
+        },
+        setHeight     :(paramAltura)=>{
+            $('#'+self.id).
+            css({
+                height:paramAltura
+            });
+        },
+        setWidth      :(paramLargura)=>{
+            $('#'+self.id).
+            css({
+                width:paramLargura
+            });
+        },
+        setBgPanel    :()=>{
+            $("#"+self.id+" > .painel").css()
+        }
+    }
+    //garante que ao inicializar as tags html ja seja adicionadas ao corpo do documanto.
+    self.construct();
+    // garante que a tela inicialise minimizada
+    $("#"+self.id).hide();
+    // retorna o obejeto
+    return (self);
+}
+
